7d8490767a11a203c37d9b1b4de904619808a4e6
flavioavilasilva <flavio.avila.silva@outlook.com>
2022-08-15 03:44:15 -0300

    Executa o service utilizado na api de criação de usuário

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
-    
+  devise_for :users,
+          controllers: {
+            registrations: 'registrations'
+          }
   devise_scope :user do
     authenticated :user do
       root 'users#index', as: :authenticated_root
